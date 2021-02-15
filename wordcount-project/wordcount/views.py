fbe73159013831c05fceb5aa813ff9ee4ee79def
Stephen Reynolds <stephenreynolds@Stephens-MacBook-Pro.local>
2021-02-15 14:59:28 -0500

    completed word count project, with Office quote

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
     return render(request, 'home.html')
@@ -9,5 +10,19 @@ def count(request):
 
     wordlist = fulltext.split()
 
-    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist)})
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+            #Increase
+            worddictionary[word] += 1
+        else:
+            #add to the dictionary
+            worddictionary[word] = 1
+        
+        sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist),'sortedwords':sortedwords})
     
+def about(request):
+    return render(request, 'about.html')
